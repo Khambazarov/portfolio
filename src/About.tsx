03ab7c58ec304c1f0e5e88d42c3eca0d117099c3
Khambazarov <khambazarovrenat@gmail.com>
2024-06-10 17:20:13 +0200

    update

--- src/About.tsx
+++ src/About.tsx
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <div className="flex min-h-screen flex-col items-center bg-gray-900 text-white">
       <h1 className="my-5 text-4xl font-bold text-center">About Me</h1>
-      <p className="px-4"></p>
+      <p className="px-4">I'm Renat</p>
     </div>
   );
 };
