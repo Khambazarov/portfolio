bcd2f257d99e0068118d1322ebf05890ff649150
Khambazarov <khambazarovrenat@gmail.com>
2024-07-02 15:48:20 +0200

    change style: home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,25 +7,25 @@ export const Home = () => {
       <h1 className="mb-5 animate-pulse text-4xl font-bold text-white">
         Welcome to my Application
       </h1>
-      <p className="mb-5 text-lg text-gray-300">
+      {/* <p className="mb-5 text-lg text-gray-300">
         This application offers various features to manage your tasks. You can
         create projects, manage contacts, and much more.
-      </p>
-      <div className="flex flex-wrap justify-center">
+      </p> */}
+      <div className="flex flex-wrap justify-center gap-x-4">
         <Link
           to="/projects"
-          className="m-2 rounded bg-teal-500 px-4 py-2 text-center text-gray-900 transition-colors duration-300 hover:bg-teal-400"
+          className="w-44 rounded bg-teal-500 px-4 py-2 text-center text-gray-900 transition-colors duration-300 hover:bg-teal-400"
         >
           Explore my Projects
         </Link>
         <Link
           to="/contact"
-          className="m-2 rounded bg-teal-500 px-4 py-2 text-center text-gray-900 transition-colors duration-300 hover:bg-teal-400"
+          className="w-44 rounded bg-teal-500 px-4 py-2 text-center text-gray-900 transition-colors duration-300 hover:bg-teal-400"
         >
           Contact Me
         </Link>
       </div>
-      <div className="flex justify-center gap-x-6 mt-1">
+      <div className="flex justify-center gap-x-36 mt-6">
         <a
           href="https://www.linkedin.com/feed"
           target="_blank"
